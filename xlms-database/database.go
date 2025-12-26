71a0665f446746e9e6510b1f69b381290d27cac5
Atharv Yadav <atharv.yadav@xperate.com>
2025-12-26 07:20:55 +0530

    database connection

---
+++ xlms-database/database.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+var Pool *pgxpool.Pool
+
+func ConnectDb() {
+	ctx := context.Background()
+
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		panic("Failed to load env variables")
+	}
+	connStr := os.Getenv("DB_URL")
+	Pool, err = pgxpool.New(ctx, connStr)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+	}
+}
