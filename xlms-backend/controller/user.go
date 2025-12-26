39b6ad393d21fefb512cf1c227fe79b769759e94
Atharv Yadav <atharv.yadav@xperate.com>
2025-12-26 18:36:02 +0530

    refactring user crub operation

---
+++ xlms-backend/controller/user.go
@@ -0,0 +1,9 @@
+package controller
+
+import (
+	"net/http"
+)
+
+func RegisterUser(w http.ResponseWriter, r *http.Response) {
+
+}
--- xlms-backend/main.go
+++ xlms-backend/main.go
@@ -7,5 +7,4 @@ import (
 func main() {
 	db.ConnectDb()
 	defer db.CloseConnection()
-
 }
